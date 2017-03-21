eba26249ed88066f8b7fc68f29ff9c5d32312520
aluno <aluno@LAB202-17>
2017-03-20 21:54:45 -0300

    adicionando novas classes

---
+++ src/Negocio/NTipoAssociado.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Negocio;
+
+import Persistencia.PTipoAssociado;
+
+/**
+ *
+ * @author aluno
+ */
+public class NTipoAssociado {
+    
+    PTipoAssociado ptipoassociado;
+    
+    public NTipoAssociado() {
+        ptipoassociado = new PTipoAssociado();
+    }
+    //
+    
+}
